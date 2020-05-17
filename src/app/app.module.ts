3c8d051b247e20205b733dfd990dc261f2eeff42
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-05-17 23:04:51 +0200

    Lista la vista individual de héroe

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// RUTAS
+import { APP_ROUTING } from './app.routes';
+
+// SERVICIOS
+import { HeroesService } from './services/heroes.service';
+
+// COMPONENTES
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroComponent } from './components/hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, APP_ROUTING],
+  providers: [HeroesService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
